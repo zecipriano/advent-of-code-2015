a82b563d292eb30f25dbf2c3d60e038a9eb4938f
Zé Cipriano <ze@zecipriano.com>
2022-10-25 14:52:59 +0100

    Install PHPUnit

---
+++ tests/DefaultTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DefaultTest extends TestCase
+{
+    public function test_default(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
